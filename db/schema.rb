e8cbf95c3b311a039371284c1e5a8c800e343b75
ryota19 <dance10113@icloud.com>
2020-08-03 14:59:10 +0900

    not null post db

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_02_085102) do
+ActiveRecord::Schema.define(version: 2020_08_03_052323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.text "text"
+    t.text "text", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
