ed1aa39bdc6d3d0e1848c58617b0d4a65ed51cc3
ryota19 <dance10113@icloud.com>
2020-08-03 16:06:15 +0900

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,55 @@
 class UsersController < ApplicationController
-  def new
-  end
-
-  def edit
+  def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
-  def index
+  def new
+    @user = User.new
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice]= "ユーザーを作成しました"
+      redirect_to user_url(@user)
+    else
+      render :new
+    end
+
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:notice] = "ユーザーを更新しました"
+      redirect_to user_url(@user)
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "ユーザーを削除しました"
+    redirect_to users_url
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/edit'
-  get 'users/show'
-  get 'users/index'
+  resources :users
+
   root to: 'posts#index'
   resources :posts 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
