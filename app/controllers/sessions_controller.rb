17e1ec401df757c0f71fd8973ac90a6a6396e4e9
ryota19 <dance10113@icloud.com>
2020-08-04 14:11:27 +0900

    sessions created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,28 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: session_params[:email])
+
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.user_id
+      flash[:notice] = "ログインしました"
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    reset_session
+    flash[:notice] = "ログアウトしました"
+    redirect_to root_url
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'login', to 'sessions#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
-  get 'sessions/new'
   resources :users
 
   root to: 'posts#index'
