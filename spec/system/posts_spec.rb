3010b0ebef12536ded1b3c232fc16e2a4cf1c3e7
ryota19 <dance10113@icloud.com>
2020-08-06 13:20:04 +0900

    posts_spec

---
+++ spec/system/posts_spec.rb
@@ -0,0 +1,19 @@
+require 'rails helper'
+
+describe '投稿管理機能', type: :system do
+    describe '一覧表示機能' do
+        before do
+            
+        end
+
+        context 'ユーザーがログインしているとき' do
+            before do
+                
+            end
+    
+            it 'ユーザーが投稿に表示される' do
+                
+            end
+        end
+    end
+end
