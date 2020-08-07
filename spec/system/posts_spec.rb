0d02e14ac9ba1b6aaa2c85ffb10a0b77733e63d0
ryota19 <dance10113@icloud.com>
2020-08-07 14:36:39 +0900

    let spec

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,37 +1,30 @@
 require 'rails_helper'
 
 describe '投稿管理機能', type: :system do
-    describe '一覧表示機能' do
-        before do
-            user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com', password: 'password')
+    describe '一覧表示機能'  do
+        let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com', password: 'password') }
+        let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com', password: 'password') }
+
+        before '各々のログイン' do
             FactoryBot.create(:post, text: '最初の投稿', user: user_a)
+            visit login_path
+            fill_in 'session_email', with: login_user.email
+            fill_in 'session_password', with: login_user.password
+            click_button 'ログインする'
         end
 
         context 'ユーザーAがログインしているとき' do
+            let(:login_user) { user_a }
 
-            
-            before do
-                visit login_path
-                fill_in 'session_email', with: 'a@example.com'
-                fill_in 'session_password', with: 'password'
-                click_button 'ログインする'
-            end
-    
-            it 'ユーザーが作成した投稿が表示される' do
+            it 'ユーザーAが作成した投稿が表示される' do
                 expect(page).to have_text '最初の投稿'
             end
         end
 
         context 'ユーザーBがログインしているとき' do
-            before do
-                FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com', password: 'password')
-                visit login_path
-                fill_in 'session_email', with: 'b@example.com'
-                fill_in 'session_password', with: 'password'
-                click_button 'ログインする'
-            end
+            let(:login_user) { user_b }
 
-            it 'ユーザーAが作成したタスクが表示される' do
+            it 'ユーザーAが作成した投稿が表示されない' do
                 expect(page).to have_text '最初の投稿'
             end
         end
