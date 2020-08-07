1f1c357562eb5706afcb6ea6f30f139db234b36b
ryota19 <dance10113@icloud.com>
2020-08-07 15:18:05 +0900

    it_behaves_like test

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -13,23 +13,22 @@ describe '投稿管理機能', type: :system do
             click_button 'ログインする'
         end
 
-        
+        shared_examples_for 'ユーザーAが作成した投稿が表示される' do
+            it { expect(page).to have_text '最初の投稿' }
+        end
 
     describe '一覧表示機能'  do
         context 'ユーザーAがログインしているとき' do
             let(:login_user) { user_a }
 
-            it 'ユーザーAが作成した投稿が表示される' do
-                expect(page).to have_text '最初の投稿'
-            end         
+            it_behaves_like 'ユーザーAが作成した投稿が表示される'         
         end
 
         context 'ユーザーBがログインしているとき' do
             let(:login_user) { user_b }
 
-            it 'ユーザーAが作成した投稿が表示されない' do
-                expect(page).to have_text '最初の投稿'
-            end
+            
+            it_behaves_like 'ユーザーAが作成した投稿が表示される'
         end
     end
 
@@ -41,9 +40,7 @@ describe '投稿管理機能', type: :system do
                 visit post_path(post_a)
             end
 
-            it 'ユーザーAが作成したとき投稿が表示される' do
-                expect(page).to have_text '最初の投稿'
-            end
+            it_behaves_like 'ユーザーAが作成した投稿が表示される'
         end
     end
 end
