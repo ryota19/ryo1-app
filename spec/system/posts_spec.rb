e57817c21edc4e733cee5212c12b595621095a65
ryota19 <dance10113@icloud.com>
2020-08-07 15:07:51 +0900

    syousai toukou

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 describe '投稿管理機能', type: :system do
-    describe '一覧表示機能'  do
         let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com', password: 'password') }
         let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com', password: 'password') }
+        let!(:post_a) { FactoryBot.create(:post, text: '最初の投稿', user: user_a) }
 
         before '各々のログイン' do
             FactoryBot.create(:post, text: '最初の投稿', user: user_a)
@@ -13,18 +13,35 @@ describe '投稿管理機能', type: :system do
             click_button 'ログインする'
         end
 
+        
+
+    describe '一覧表示機能'  do
         context 'ユーザーAがログインしているとき' do
             let(:login_user) { user_a }
 
             it 'ユーザーAが作成した投稿が表示される' do
                 expect(page).to have_text '最初の投稿'
-            end
+            end         
         end
 
         context 'ユーザーBがログインしているとき' do
             let(:login_user) { user_b }
 
             it 'ユーザーAが作成した投稿が表示されない' do
+                expect(page).to have_text '最初の投稿'
+            end
+        end
+    end
+
+    describe '詳細表示機能' do
+        context 'ユーザーAがログインいているとき' do
+            let(:login_user)  { user_a }
+
+            before do
+                visit post_path(post_a)
+            end
+
+            it 'ユーザーAが作成したとき投稿が表示される' do
                 expect(page).to have_text '最初の投稿'
             end
         end
