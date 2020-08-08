ab54ff09fa487e89eee0b12019a1d6cf9dce874c
ryota19 <dance10113@icloud.com>
2020-08-08 18:34:43 +0900

    logout test re

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
-feature 'ログイン機能' do
-    background do
-        User.create!(name: 'user_a', email: 'a@example.com', password: 'password' )
-    end
+feature 'ログイン機能'  do
 
-    scenario 'ログインする' do
+    background do
+        User.create!(name: 'user_a', email: 'a@example.com', password: 'password')   
+    end    
+
+    scenario 'ログイン'  do
         visit root_path
         fill_in 'session_email', with: 'a@example.com'
         fill_in 'session_password', with: 'password'
-        click_on 'ログイン'
+        click_button 'ログイン'
 
         expect(page).to have_content 'ログインしました'
     end
 
     scenario 'ログアウト' do
-       visit root_path
-       fill_in 'session_email', with: 'a@example.com'
-       fill_in 'session_password', with: 'password'
-       click_on 'ログイン'
-       click_on 'logout'
+        visit root_path
+        fill_in 'session_email', with: 'a@example.com'
+        fill_in 'session_password', with: 'password'
+        click_button 'ログイン'
+        click_on 'logout'
 
-       expect(page).to have_content 'ログアウトしました'
+        expect(page).to have_content 'ログアウトしました'
     end
 end
-
