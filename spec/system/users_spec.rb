15a6f4a777486ad265edf31384b37cae3c4b7559
ryota19 <dance10113@icloud.com>
2020-08-09 14:25:40 +0900

    edit test

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,26 +2,40 @@ require 'rails_helper'
 
 feature 'ログイン機能', type: :system do
 
-    background do
-        User.create!(name: 'user_a', email: 'a@example.com', password: 'password')   
+    background do  
+        user_a = FactoryBot.create(:user, name: 'user_a', email: 'a@example.com', password: 'password')
+        FactoryBot.create(:post, text: '最初の投稿', user: user_a)
+
+        visit root_path
+            fill_in 'session_email', with: 'a@example.com'
+            fill_in 'session_password', with: 'password'
+            click_button 'ログイン' 
     end    
 
-    scenario 'ログイン'  do
-        visit root_path
-        fill_in 'session_email', with: 'a@example.com'
-        fill_in 'session_password', with: 'password'
-        click_button 'ログイン'
 
-        expect(page).to have_content 'ログインしました'
+    context 'ログイン' do
+        scenario 'ログイン'  do
+            expect(page).to have_content 'ログインしました'
+        end
+    end
+    context 'ログアウト' do
+        scenario 'ログアウト' do
+            click_on 'logout'
+            expect(page).to have_content 'ログアウトしました'
+        end
+    end
+   
+    context 'アカウント編集' do
+        scenario 'アカウント編集' do
+            visit users_path
+            click_on 'user_a'
+            fill_in 'session_password2', with: 'password'
+            fill_in 'session_passwordconfir', with: 'password'
+            click_button '更新する'
+            
+            expect(page).to have_content 'ユーザーを更新しました'
+        end
     end
 
-    scenario 'ログアウト' do
-        visit root_path
-        fill_in 'session_email', with: 'a@example.com'
-        fill_in 'session_password', with: 'password'
-        click_button 'ログイン'
-        click_on 'logout'
-
-        expect(page).to have_content 'ログアウトしました'
-    end
-end
+    
+end 
